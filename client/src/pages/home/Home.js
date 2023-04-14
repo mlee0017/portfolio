bdf31c001e6e4ce1adb56ecd3316f552bc7ecab7
Michelle Lee <lee.mxvii@gmail.com>
2023-04-13 21:35:59 -0500

    links for projects

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -4,13 +4,13 @@ function Home () {
             home page
             <ul>
                 <li>
-                    <a href="">project 1</a>
+                    <a href="https://github.com/mlee0017/mod-1">project 1</a>
                 </li>
                 <li>
-                    <a href="">project 2</a>
+                    <a href="https://github.com/mlee0017/mod-2">project 2</a>
                 </li>
                 <li>
-                    <a href="">project 3</a>
+                    <a href="https://github.com/mlee0017/mod-3/tree/master">project 3</a>
                 </li>
             </ul>
         </div>
